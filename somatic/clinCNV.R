0826c4ab2175128405a6a561771d581961392d89
German Demidov <ahdemig1@SRV018.img.med.uni-tuebingen.de>
2019-01-25 11:06:42 +0100

    Fixed bug with germline_sample_no

--- somatic/clinCNV.R
+++ somatic/clinCNV.R
@@ -185,7 +185,7 @@ if (length(whichBedIsNA) > 0)
 
 #normal <- read.table(opt$normal, header=T, stringsAsFactors = F, comment.char="&" )
 normal <- ReadFileFast(opt$normal, header=T)
-normal = normal[,1:10]
+normal = normal
 colnames(normal) = cutX(colnames(normal))
 if (!startsWith(normal[,1], "chr"))
   normal[,1] <- paste0("chr", normal[,1])
