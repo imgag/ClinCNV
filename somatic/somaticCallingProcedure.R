8ccaa7067707593557409b451ca50fc1a0207ece
GermanDemidov <german.m.demidov@gmail.com>
2019-07-11 14:02:16 +0200

    QC control for somatic samples. Headers.

--- somatic/somaticCallingProcedure.R
+++ somatic/somaticCallingProcedure.R
@@ -903,7 +903,7 @@ somaticCalling <- function(matrixOfLogFold) {
       fileToOut <- paste0(folder_name, sample_name, paste0("/CNAs_", sample_name, ".txt"))
       fileConn<-file(fileToOut)
       ploidyEst = round(2 + (2 * 2 ** median(matrixOfLogFold[which(!bedFileForCluster[,1] %in% c("chrX", "chrY")),sam_no]) - 2) / ifelse(max(local_purities) == 0, 1, max(local_purities)), 4)
-      writeLines(c(paste0("##ANALYSISTYPE=CLINCNV_TUMOR_NORMAL_PAIR"), paste0( "##", clincnvVersion), paste0("##Analysis finished on ", Sys.time()),  paste("##"," QC ", QC_value, ", gender of sample:", genderOfSamples[germline_sample_no], "ploidy: ", ploidyEst, "clonality by BAF (if != 1):", paste(round(unique(local_purities), digits=3), collapse=";"), collapse = " ")), fileConn)
+      writeLines(c(paste0("##ANALYSISTYPE=CLINCNV_TUMOR_NORMAL_PAIR"), paste0( "##", clincnvVersion), paste0("##Analysis finished on ", Sys.time()),  paste("##"," QC ", round(QC_value, 5), ", gender of sample:", genderOfSamples[germline_sample_no], "ploidy: ", ploidyEst, "clonality by BAF (if != 1):", paste(round(unique(local_purities), digits=3), collapse=";"), collapse = " ")), fileConn)
       close(fileConn)
       
       if(opt$debug) {
