732ce374f3ef7265574533817b4bb61bb8fd0072
GermanDemidov <german.m.demidov@gmail.com>
2019-08-01 16:09:23 +0200

    Removed one additional space

--- somatic/somaticCallingProcedure.R
+++ somatic/somaticCallingProcedure.R
@@ -904,7 +904,8 @@ somaticCalling <- function(matrixOfLogFold) {
       fileConn<-file(fileToOut)
       ploidyEst = round(2 + (2 * 2 ** median(matrixOfLogFold[which(!bedFileForCluster[,1] %in% c("chrX", "chrY")),sam_no]) - 2) / ifelse(max(local_purities) == 0, 1, max(local_purities)), 4)
       estimatedFDR = ifelse(QC_value == "NA", "NA", round(QC_value, 5))
-      writeLines(c(paste0("##ANALYSISTYPE=CLINCNV_TUMOR_NORMAL_PAIR"), paste0( "##", clincnvVersion), paste0("##Analysis finished on: ", Sys.time()),  paste0("##estimated fdr: ", estimatedFDR), paste0("##gender of sample: ", genderOfSamples[germline_sample_no]), paste0("##ploidy: ", ploidyEst), paste0("##clonality by BAF (if != 1): ", paste(round(unique(local_purities), digits=3), collapse=";"), collapse = " ")), fileConn)      close(fileConn)
+      writeLines(c(paste0("##ANALYSISTYPE=CLINCNV_TUMOR_NORMAL_PAIR"), paste0( "##", clincnvVersion), paste0("##Analysis finished on: ", Sys.time()),  paste0("##estimated fdr: ", estimatedFDR), paste0("##gender of sample: ", genderOfSamples[germline_sample_no]), paste0("##ploidy: ", ploidyEst), paste0("##clonality by BAF (if != 1): ", paste(round(unique(local_purities), digits=3), collapse=";"), collapse = " ")), fileConn)     
+      close(fileConn)
       
       if(opt$debug) {
         print(found_CNVs_total)
