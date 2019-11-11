f98cf73dd055fe530d42f923938ba7d0f6ae192e
GermanDemidov <german.m.demidov@gmail.com>
2019-11-11 08:46:01 +0100

    Fixed bug with small panels

--- clinCNV.R
+++ clinCNV.R
@@ -253,7 +253,7 @@ numberOfElemsInEachChromosome = sapply(1:length(presentedChromsOn), function(i) 
 
 for (i in 1:20) {
   tableOfValues <- table(round(as.numeric(as.character(bedFile[,4])) / i, digits = 2) * i)
-  if(sum(tableOfValues[which(tableOfValues > 20)]) / sum(tableOfValues) > 0.95) break 
+  if(sum(tableOfValues[which(tableOfValues > 25)]) / sum(tableOfValues) > 0.95) break 
 }
 bedFile[,4] <- round(as.numeric(as.character(bedFile[,4])) / i, digits = 2) * i
 whichBedIsNA <- which(is.na(bedFile[,4]) | bedFile[,3] - bedFile[,2] < 50 | (!bedFile[,1] %in% presentedChromsOn[numberOfElemsInEachChromosome]))
