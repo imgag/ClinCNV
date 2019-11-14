c17a21b67f5c9b49d2fd6e4aad13647eb40ebe85
Timofei Bondarev <bondarevts@gmail.com>
2019-11-14 10:02:39 -0500

    Renames in baf_extractor.py

--- helper_scripts/baf_extractor.py
+++ helper_scripts/baf_extractor.py
@@ -9,42 +9,42 @@ def vcf_parse_one_line(line: str, minimum_quality: float) -> Optional[List]:
     if line.startswith("#"):
         return None
 
-    format_fields = line.split("\t")[8].split(":")
-    index_for_genotype = format_fields.index("GT")
-    index_for_depth = format_fields.index("DP")
+    sample_data_types = line.split("\t")[8].split(":")
+    genotype_index = sample_data_types.index("GT")
+    depth_index = sample_data_types.index("DP")
     try:
-        index_for_other_depth = format_fields.index("AO")
+        other_depth_index = sample_data_types.index("AO")
     except ValueError:
-        index_for_other_depth = -1
+        other_depth_index = -1
     try:
-        index_for_frequency = format_fields.index("AF")
+        frequency_index = sample_data_types.index("AF")
     except ValueError:
-        index_for_frequency = -1
+        frequency_index = -1
 
-    splitted_line = line.split("\t")
-    chrom = splitted_line[0]
-    start = splitted_line[1]
-    end = splitted_line[1]
-    reference_variant = splitted_line[3]
-    allelic_variant = splitted_line[4].split(",")
+    vcf_fields = line.split("\t")
+    chromosome = vcf_fields[0]
+    start = vcf_fields[1]
+    end = vcf_fields[1]
+    reference_variant = vcf_fields[3]
+    allelic_variant = vcf_fields[4].split(",")
     if len(allelic_variant[0]) > 1 or len(reference_variant) > 1 or len(allelic_variant) > 2:
         return None
-    quality = float(splitted_line[5])
+    quality = float(vcf_fields[5])
     if quality < minimum_quality:
         return None
-    baf_depth = splitted_line[9].strip().split(":")
-    if baf_depth[index_for_genotype] == "0/1":
-        depth = baf_depth[index_for_depth]
-        if index_for_frequency > 0:
-            freq = baf_depth[index_for_frequency]
-        elif index_for_depth > 0:
-            freq = float(baf_depth[index_for_other_depth]) / int(depth)
-        else:
-            sys.exit("No AO or AF fields in VCF format column. Quit.")
-        if int(depth) > 1:
-            return [chrom, start, end, reference_variant, allelic_variant[0], chrom + "_" + start,
-                    freq, depth]
-    return None
+    baf_depth = vcf_fields[9].strip().split(":")
+    if baf_depth[genotype_index] != "0/1":
+        return None
+    depth = baf_depth[depth_index]
+    if frequency_index > 0:
+        freq = baf_depth[frequency_index]
+    elif depth_index > 0:
+        freq = float(baf_depth[other_depth_index]) / int(depth)
+    else:
+        sys.exit("No AO or AF fields in VCF format column. Quit.")
+    if int(depth) > 1:
+        return [chromosome, start, end, reference_variant, allelic_variant[0], chromosome + "_" + start,
+                freq, depth]
 
 
 def parse_vcf(input_file_name: str, output_file_name: str, minimum_quality: float) -> None:
