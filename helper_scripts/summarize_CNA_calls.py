97819742b9b82cc29e4ad954484125ceaef70eac
Timofei Bondarev <bondarevts@gmail.com>
2019-11-29 23:12:25 -0500

    Use fdr_threshold for BAF_qval_fdr instead of constant 0.05

--- helper_scripts/summarize_CNA_calls.py
+++ helper_scripts/summarize_CNA_calls.py
@@ -94,9 +94,8 @@ def clean_file(file: IO[str], output_path: Path, fdr_threshold: float, sample: S
             if len(fields) == 1:
                 continue
 
-            allele_balance = major_cn_allele == minor_cn_allele
-            secondary_allele_balance = major_cn_allele2 == minor_cn_allele2
-            if allele_balance and secondary_allele_balance or BAF_qval_fdr == "NA" or float(BAF_qval_fdr) < 0.05:
+            alleles_balance = major_cn_allele == minor_cn_allele and major_cn_allele2 == minor_cn_allele2
+            if alleles_balance or BAF_qval_fdr == "NA" or float(BAF_qval_fdr) < fdr_threshold:
                 lines.append(line)
                 continue
 
